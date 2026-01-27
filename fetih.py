20ec506477b055ea9c1abdc6c6d953f53c1d0cc4
Muhammed Fatih Uğurlu <muhammedfatihugurlu10@gmail.com>
2026-01-27 21:51:56 +0300

    1

--- fetih.py
+++ fetih.py
@@ -21,20 +21,17 @@ st.set_page_config(
 # Senin ikonun GitHub linki
 icon_url = "https://raw.githubusercontent.com/MuhammedFatihUgurlu/FetihAI/main/icon-512.png"
 
-st.markdown("""
-    <style>
-    /* Bu kısım ekrandaki gereksiz boşlukları da siler */
-    .stApp {
-        margin-top: -50px;
-    }
-    </style>
-    
+st.markdown(f"""
     <head>
+        <link rel="icon" type="image/png" href="{icon_url}">
+        <link rel="shortcut icon" type="image/png" href="{icon_url}">
+        
+        <link rel="apple-touch-icon" href="{icon_url}">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        
         <meta name="mobile-web-app-capable" content="yes">
-        <meta name="theme-color" content="#0E1117">
-        <link rel="icon" sizes="192x192" href="https://raw.githubusercontent.com/MuhammedFatihUgurlu/FetihAI/main/icon-512.png">
-        <link rel="icon" sizes="512x512" href="https://raw.githubusercontent.com/MuhammedFatihUgurlu/FetihAI/main/icon-512.png">
-        <link rel="apple-touch-icon" href="https://raw.githubusercontent.com/MuhammedFatihUgurlu/FetihAI/main/icon-512.png">
+        <meta name="theme-color" content="#0E1117"> <link rel="icon" sizes="192x192" href="{icon_url}">
+        <link rel="icon" sizes="512x512" href="{icon_url}">
     </head>
     """, unsafe_allow_html=True)
 
